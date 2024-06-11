0878dac34ffdbbe08b34cecf494b482c00494098
Ricardo Segura <seguraricardo380@gmail.com>
2024-06-11 14:30:31 -0500

    errors: Commit antes de usar el code de Kevin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from analisis.analisisCalidadAire import *
-from analisis.analisisCalidadRuido import *
+#from analisis.analisisCalidadRuido import *
 
-
-from analisis.analisisSistemaArboles import *
-from analisis.analisisSostenibilidad import *
+#from analisis.analisisSistemaArboles import *
+#from analisis.analisisSostenibilidad import *
 #from analisis.analisisTransporte import *
