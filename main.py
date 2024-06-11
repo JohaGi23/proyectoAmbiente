8b9a7c7df85104b67c9d85dde10e945df4654f36
Ricardo Segura <seguraricardo380@gmail.com>
2024-06-11 15:14:57 -0500

    Fix: Proyecto v2 OK

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from analisis.analisisCalidadAire import *
 from analisis.analisisCalidadRuido import *
 
-from analisis.analisisSistemaArboles import *
-from analisis.analisisSostenibilidad import *
-from analisis.analisisTransporte import *
+#from analisis.analisisSistemaArboles import *
+#from analisis.analisisSostenibilidad import *
+#from analisis.analisisTransporte import *
