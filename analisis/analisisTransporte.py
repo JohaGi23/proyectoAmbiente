dec244856f3cbbceefa912c9177c3daef859cae3
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-06-12 20:38:14 -0500

    Actualizacion Graficos SistemaArboles y transporte

--- analisis/analisisTransporte.py
+++ analisis/analisisTransporte.py
@@ -7,20 +7,20 @@ from helpers.generadorTabla import crearTablaHtml
 
 def construirDataFrameTransporte():
     #traigo los datos generados en el mock
-    datosTransporte=generadorDatosTransporte()
+   datosTransporte=generadorDatosTransporte()
 
     #construyo el Dataframe
-    TransporteDF=pd.DataFrame(datosTransporte,columns=['Medio','Frecuencia','Genero','Id','Nombre'])
+   TransporteDF=pd.DataFrame(datosTransporte,columns=['Medio','Frecuencia','Genero','Id','Nombre'])
 
      #limpiando el dataframe
 
   #1. Limpiando (reemplazando valores)
-  TransporteDF.replace('-', pd.NA, inplace=True)
-  TransporteDF.replace('sin', pd.NA, inplace=True)
+   TransporteDF.replace('-', pd.NA, inplace=True)
+   TransporteDF.replace('sin', pd.NA, inplace=True)
 
   #2. limpiando (eliminando valores)
-  TransporteDF.replace('sin', pd.NA, inplace=True)
-  TransporteDF.dropna(inplace=True)
+   TransporteDF.replace('sin', pd.NA, inplace=True)
+   TransporteDF.dropna(inplace=True)
 
   #3. Filtrando datos para depurar la informacion
   #FILTRAR DTOS ES OBTENER NUEVOS DATFRAMES
@@ -38,27 +38,30 @@ def construirDataFrameTransporte():
   #print(filtroFrecuenciaModerado)
   #print('\n')
   # print(flitroFrecuenciaPeligroso)
-  print('\n')
+   print('\n')
 
   #agrupando datos
-  datosAgrupados=TransporteDF.groupby("Medio")["Frecuencia"].mean()
+   datosAgrupados=TransporteDF.groupby("Medio")["Frecuencia"].mean()
 
-  #graficando datos
-  plt.figure(figsize=(20,20))
-  datosAgrupados.plot(kind='bar',color='green')
-  plt.title('Frecuencia de Medio de Transporte utilizado en Medellín')
-  plt.xlabel('Medio')
-  plt.ylabel('Frecuencia')
-  plt.grid(True)
-  plt.xticks(rotation=45)
-  plt.savefig('./assets/img/FrecuenciaT.png',format='png',dpi=300)
-  plt.show()
+      #graficando datos
+   plt.figure(figsize=(20,20))
+   datosAgrupados.plot(kind='bar',color='green')
+   plt.title('Frecuencia de Medio de Transporte utilizado en Medellín')
+   plt.xlabel('Medio')
+   plt.ylabel('Frecuencia')
+   plt.grid(True)
+   plt.xticks(rotation=45)
+   plt.savefig('./assets/img/FrecuenciaT.png',format='png',dpi=300)
+   plt.show()
 
-  #probando...
-  
-  crearTablaHtml(TransporteDF,"Transporte")
+      #probando...
+      
+   try:
+    crearTablaHtml(TransporteDF, "transporte")
+   except Exception as e:
+    print("Error al crear la tabla HTML:", e)
 
-    #probando...
-print(TransporteDF)
+         #probando...
+   print(TransporteDF)
 
 construirDataFrameTransporte()
